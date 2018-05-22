2240fc7804556a9e034a9ac6c397b32cd0dffd0c
IsaAliev <isaaliev12@gmail.com>
2018-05-22 17:26:03 +0300

    Create test app

---
+++ GIBLibTestApp/UITracking.framework/Versions/A/Headers/UITextView+Tracking.h
@@ -0,0 +1,15 @@
+//
+//  UITextView+Tracking.h
+//  GIBTask
+//
+//  Created by Isa Aliev on 22.05.2018.
+//  Copyright © 2018 IA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSObject+Swizzling.h"
+
+@interface UITextView (Tracking)
+
+@end
